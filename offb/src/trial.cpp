da45b7cefe3fdd861fd84a373820a88b6e99f5ea
pattylo <19451945as@gmail.com>
2022-06-04 14:31:02 +0800

    first commit

---
+++ offb/src/trial.cpp
@@ -0,0 +1,34 @@
+#include <ros/ros.h>
+#include "include/roshandle.hpp"
+
+
+ros::Publisher pub;
+
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
+{
+  // Create a container for the data.
+  sensor_msgs::PointCloud2 output;
+
+  // Do data processing here...
+  output = *input;
+
+  // Publish the data.
+  pub.publish (output);
+}
+
+int main (int argc, char** argv)
+{
+  // Initialize ROS
+  ros::init (argc, argv, "my_pcl_tutorial");
+  ros::NodeHandle nh;
+
+  // // Create a ROS subscriber for the input point cloud
+  // ros::Subscriber sub = nh.subscribe ("input", 1, cloud_cb);
+
+  // // Create a ROS publisher for the output point cloud
+  // pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+
+  // // Spin
+  // ros::spin ();
+
+}
