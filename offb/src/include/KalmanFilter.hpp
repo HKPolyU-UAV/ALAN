34b7480c32d6c41d2eaef8120b5cc0dfd893a061
pattylo <19451945as@gmail.com>
2022-06-05 16:37:25 +0800

    just commit

--- offb/src/include/KalmanFilter.hpp
+++ offb/src/include/KalmanFilter.hpp
@@ -11,6 +11,13 @@ class KalmanFilter
 private:
     Eigen::MatrixXd Fk, Pk, Pk_bar, Qk, Hk, Rk, xk;
     int state_size, mea_size;
+    kf_type type;
+    void lkf_predict(Eigen::VectorXd& x, float dt);
+    void lkf_update(Eigen::VectorXd& x, Eigen::VectorXd z);
+    void ekf_predict(Eigen::VectorXd& x, float dt);
+    void ekf_update(Eigen::VectorXd& x, Eigen::VectorXd z);
+    void ukf_predict(Eigen::VectorXd& x, float dt);
+    void ukf_update(Eigen::VectorXd& x, Eigen::VectorXd z);
 public:
     KalmanFilter(
         Eigen::MatrixXd F, 
@@ -51,11 +58,36 @@ KalmanFilter::KalmanFilter(
     this->Rk = R;
     this->state_size = dim[0];
     this->mea_size   = dim[1];
+    this->type       = which;
 }
 
 KalmanFilter::~KalmanFilter(){}
 
 void KalmanFilter::predict(Eigen::VectorXd& x, float dt)
+{
+    if(type == linear)
+        lkf_predict(x, dt);
+    else if (type == extended)
+        ekf_predict(x, dt);
+    else if (type == unscented)
+        ukf_predict(x, dt);
+    else
+        cout<< "Please identify Kalman filter type!" <<endl;
+}
+
+void KalmanFilter::update(Eigen::VectorXd& x, Eigen::VectorXd zk)
+{
+    if(type == linear)
+        lkf_update(x, zk);
+    else if (type == extended)
+        ekf_update(x, zk);
+    else if (type == unscented)
+        ukf_update(x, zk);
+    else
+        cout<< "Please identify Kalman filter type!" <<endl;
+}
+
+void KalmanFilter::lkf_predict(Eigen::VectorXd& x, float dt)
 {
     Eigen::MatrixXd F(state_size, state_size);
     F << 1, 0, 0, dt,  0,  0,
@@ -70,7 +102,63 @@ void KalmanFilter::predict(Eigen::VectorXd& x, float dt)
     x = xk;
 }
 
-void KalmanFilter::update(Eigen::VectorXd& x, Eigen::VectorXd zk)
+void KalmanFilter::lkf_update(Eigen::VectorXd& x, Eigen::VectorXd zk)
+{
+    Eigen::MatrixXd Sk = Hk * Pk_bar * Hk.transpose() + Rk; // system uncertainty
+    Eigen::MatrixXd Kk = Pk_bar * Hk * Sk.inverse(); //Kalman Gain
+    Eigen::VectorXd yk = zk - Hk * x; //residual
+
+    x = x + Kk * yk;
+    
+    Eigen::MatrixXd I_KH = Eigen::MatrixXd::Identity(state_size, state_size) - Kk * Hk;
+    Pk = I_KH * Pk_bar * I_KH.transpose() + Kk * Rk * Kk.transpose();
+
+}
+
+void KalmanFilter::ekf_predict(Eigen::VectorXd& x, float dt)
+{
+    Eigen::MatrixXd F(state_size, state_size);
+    F << 1, 0, 0, dt,  0,  0,
+         0, 1, 0,  0, dt,  0,
+         0, 0, 1,  0,  0, dt,
+         0, 0, 0,  1,  0,  0,
+         0, 0, 0,  0,  0,  1;
+
+    xk = F * x;
+    Pk_bar = F * Pk * F.transpose();
+    
+    x = xk;
+}
+
+void KalmanFilter::ekf_update(Eigen::VectorXd& x, Eigen::VectorXd zk)
+{
+    Eigen::MatrixXd Sk = Hk * Pk_bar * Hk.transpose() + Rk; // system uncertainty
+    Eigen::MatrixXd Kk = Pk_bar * Hk * Sk.inverse(); //Kalman Gain
+    Eigen::VectorXd yk = zk - Hk * x; //residual
+
+    x = x + Kk * yk;
+    
+    Eigen::MatrixXd I_KH = Eigen::MatrixXd::Identity(state_size, state_size) - Kk * Hk;
+    Pk = I_KH * Pk_bar * I_KH.transpose() + Kk * Rk * Kk.transpose();
+
+}
+
+void KalmanFilter::ukf_predict(Eigen::VectorXd& x, float dt)
+{
+    Eigen::MatrixXd F(state_size, state_size);
+    F << 1, 0, 0, dt,  0,  0,
+         0, 1, 0,  0, dt,  0,
+         0, 0, 1,  0,  0, dt,
+         0, 0, 0,  1,  0,  0,
+         0, 0, 0,  0,  0,  1;
+
+    xk = F * x;
+    Pk_bar = F * Pk * F.transpose();
+    
+    x = xk;
+}
+
+void KalmanFilter::ukf_update(Eigen::VectorXd& x, Eigen::VectorXd zk)
 {
     Eigen::MatrixXd Sk = Hk * Pk_bar * Hk.transpose() + Rk; // system uncertainty
     Eigen::MatrixXd Kk = Pk_bar * Hk * Sk.inverse(); //Kalman Gain
